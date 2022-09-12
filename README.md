52985b3df81568afeaef3e04e3ef4a26f37acc0d
Lucas Pinheiro <lucaspinheiroalberdanha@gmail.com>
2022-09-12 18:05:08 -0300

    Updates

---
+++ README.md
@@ -0,0 +1,5 @@
+# Complexo RJ website
+
+Site desenvolvido pela zoofle/code atoms, para o servidor GTAV RP Complexo RJ.
+
+Website developed by zoofle/code atoms, for the GTAV RP Complexo RJ server.
---
+++ script.js
@@ -0,0 +1,179 @@
+function showSalaryContainer() {
+	const salaryContainer = document.getElementById('salary-topic-tutorial');
+	if (salaryContainer.style.display === 'none') {
+		salaryContainer.style.display = 'flex';
+	} else {
+		salaryContainer.style.display = 'none';
+	}
+}
+
+function showTokovoipContainer() {
+	const tokovoipContainer = document.getElementById('tokovoip-topic-tutorial');
+	if (tokovoipContainer.style.display === 'none') {
+		tokovoipContainer.style.display = 'flex';
+	} else {
+		tokovoipContainer.style.display = 'none';
+	}
+}
+
+function showRadioContainer() {
+	const radioContainer = document.getElementById('radio-topic-tutorial');
+	if (radioContainer.style.display === 'none') {
+		radioContainer.style.display = 'flex';
+	} else {
+		radioContainer.style.display = 'none';
+	}
+}
+
+function showClothesContainer() {
+	const clothesContainer = document.getElementById('clothes-topic-tutorial');
+	if (clothesContainer.style.display === 'none') {
+		clothesContainer.style.display = 'flex';
+	} else {
+		clothesContainer.style.display = 'none';
+	}
+}
+
+function showHousesContainer() {
+	const housesContainer = document.getElementById('houses-topic-tutorial');
+	if (housesContainer.style.display === 'none') {
+		housesContainer.style.display = 'flex';
+	} else {
+		housesContainer.style.display = 'none';
+	}
+}
+
+function showLumberjackContainer() {
+	const lumberjackContainer = document.getElementById('lumberjack-topic-tutorial');
+	if (lumberjackContainer.style.display === 'none') {
+		lumberjackContainer.style.display = 'flex';
+	} else {
+		lumberjackContainer.style.display = 'none';
+	}
+}
+
+function showTrashmanContainer() {
+	const trashmanContainer = document.getElementById('trashman-topic-tutorial');
+	if (trashmanContainer.style.display === 'none') {
+		trashmanContainer.style.display = 'flex';
+	} else {
+		trashmanContainer.style.display = 'none';
+	}
+}
+
+function showMinerContainer() {
+	const minerContainer = document.getElementById('miner-topic-tutorial');
+	if (minerContainer.style.display === 'none') {
+		minerContainer.style.display = 'flex';
+	} else {
+		minerContainer.style.display = 'none';
+	}
+}
+
+function showFarmerContainer() {
+	const farmerContainer = document.getElementById('farmer-topic-tutorial');
+	if (farmerContainer.style.display === 'none') {
+		farmerContainer.style.display = 'flex';
+	} else {
+		farmerContainer.style.display = 'none';
+	}
+}
+
+function showAmazonContainer() {
+	const amazonContainer = document.getElementById('amazon-topic-tutorial');
+	if (amazonContainer.style.display === 'none') {
+		amazonContainer.style.display = 'flex';
+	} else {
+		amazonContainer.style.display = 'none';
+	}
+}
+
+function showMilkmanContainer() {
+	const milkmanContainer = document.getElementById('milkman-topic-tutorial');
+	if (milkmanContainer.style.display === 'none') {
+		milkmanContainer.style.display = 'flex';
+	} else {
+		milkmanContainer.style.display = 'none';
+	}
+}
+
+function showFruitContainer() {
+	const fruitContainer = document.getElementById('fruit-topic-tutorial');
+	if (fruitContainer.style.display === 'none') {
+		fruitContainer.style.display = 'flex';
+	} else {
+		fruitContainer.style.display = 'none';
+	}
+}
+
+function showHunterContainer() {
+	const hunterContainer = document.getElementById('hunter-topic-tutorial');
+	if (hunterContainer.style.display === 'none') {
+		hunterContainer.style.display = 'flex';
+	} else {
+		hunterContainer.style.display = 'none';
+	}
+}
+
+function showFisherContainer() {
+	const fisherContainer = document.getElementById('fisher-topic-tutorial');
+	if (fisherContainer.style.display === 'none') {
+		fisherContainer.style.display = 'flex';
+	} else {
+		fisherContainer.style.display = 'none';
+	}
+}
+
+function showDriverContainer() {
+	const driverContainer = document.getElementById('driver-topic-tutorial');
+	if (driverContainer.style.display === 'none') {
+		driverContainer.style.display = 'flex';
+	} else {
+		driverContainer.style.display = 'none';
+	}
+}
+
+const salaryButton = document.getElementById('salary-container-button');
+salaryButton.addEventListener('click', showSalaryContainer);
+
+const tokovoipButton = document.getElementById('tokovoip-container-button')
+tokovoipButton.addEventListener('click', showTokovoipContainer);
+
+const radioButton = document.getElementById('radio-container-button');
+radioButton.addEventListener('click', showRadioContainer);
+
+const clothesButton = document.getElementById('clothes-container-button');
+clothesButton.addEventListener('click', showClothesContainer);
+
+const housesButton = document.getElementById('houses-container-button');
+housesButton.addEventListener('click', showHousesContainer);
+
+const lumberjackButton = document.getElementById('lumberjack-container-button');
+lumberjackButton.addEventListener('click', showLumberjackContainer);
+
+const trashmanButton = document.getElementById('trashman-container-button');
+trashmanButton.addEventListener('click', showTrashmanContainer);
+
+const minerButton = document.getElementById('miner-container-button');
+minerButton.addEventListener('click', showMinerContainer);
+
+const farmerButton = document.getElementById('farmer-container-button');
+farmerButton.addEventListener('click', showFarmerContainer);
+
+const amazonButton = document.getElementById('amazon-container-button');
+amazonButton.addEventListener('click', showAmazonContainer);
+
+const milkmanButton = document.getElementById('milkman-container-button');
+milkmanButton.addEventListener('click', showMilkmanContainer);
+
+const fruitButton = document.getElementById('fruit-container-button');
+fruitButton.addEventListener('click', showFruitContainer);
+
+const hunterButton = document.getElementById('hunter-container-button');
+hunterButton.addEventListener('click', showHunterContainer);
+
+const fisherButton = document.getElementById('fisher-container-button');
+fisherButton.addEventListener('click', showFisherContainer);
+
+const driverButton = document.getElementById('driver-container-button');
+driverButton.addEventListener('click', showDriverContainer);
